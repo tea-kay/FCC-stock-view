0aca2ba3ef057ff94722c29f6ea84ab45570882e
Timothy Kim <tkim1202@gmail.com>
2018-02-22 19:00:14 -0500

    Formatted sizing for chart view

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import io from 'socket.io-client';
 import logo from './logo.svg';
 import './App.css';
 
-import { Grid, Row, Col, ListGroup, ListGroupItem, FormGroup, FormControl } from 'react-bootstrap';
+import { Grid, Row, Col, ListGroup, ListGroupItem, FormGroup, FormControl, PageHeader, small } from 'react-bootstrap';
 
 const socket = io.connect();
 
@@ -37,24 +37,48 @@ class App extends Component {
         <div className="container-fluid">
           <Row>
             <Col md={4} className="stock-list">
-              <form>
-                <FormGroup bsSize="large" className="row">
-                  <FormControl type="text" placeholder="Search By Stock Ticker" className="search-bar fluid"/>
-                </FormGroup>
-              </form>
-              <ListGroup className="fluid">
-                <ListGroupItem className="row">
-                  <Col md={12}><code>APPL</code><code>$PRICE</code></Col>
+              <Row>
+                <Col md={4} className="logo-sec">LOGO</Col>
+                <Col md={8}>
+                  <form>
+                    <FormGroup bsSize="large" className="row">
+                      <FormControl type="text" placeholder="Search By Stock Ticker" className="search-bar fluid"/>
+                    </FormGroup>
+                  </form>
+                </Col>
+              </Row>
+              <ListGroup className="stock-info">
+                <ListGroupItem className="row" >
+                  <Row>
+                    <Col md={6}><code>APPL</code></Col><Col md={6}><code>$PRICE</code></Col>
+                  </Row>
                 </ListGroupItem>
                 <ListGroupItem className="row">
-                  <Col md={12}><code>APPL</code><code>$PRICE</code></Col>
+                  <Row>
+                    <Col md={6}><code>APPL</code></Col><Col md={6}><code>$PRICE</code></Col>
+                  </Row>
                 </ListGroupItem>
                 <ListGroupItem className="row">
-                  <Col md={12}><code>APPL</code><code>$PRICE</code></Col>
+                  <Row>
+                    <Col md={6}><code>APPL</code></Col><Col md={6}><code>$PRICE</code></Col>
+                  </Row>
                 </ListGroupItem>
               </ListGroup>
             </Col>
             <Col md={8} className="chart-view">
+              <Row>
+                <Col md={12}>
+                <PageHeader className="header-content">
+                  Stock View App <small>Visualize The Performance of Your Favorite Stocks</small>
+                </PageHeader>
+                <br/>
+              </Col>
+              </Row>
+              <Row>
+                <Col md={12} className="live-chart">
+
+                </Col>
+              </Row>
             </Col>
           </Row>
         </div>
