b3c93bfe1b6010fcaff33a07d6bb7dddaa1b13f0
Timothy Kim <tkim1202@gmail.com>
2018-02-23 15:30:32 -0500

    Added buttons for date range

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
 import io from 'socket.io-client';
 import logo from './logo.svg';
 import './App.css';
 
-import { Grid, Row, Col, ListGroup, ListGroupItem, FormGroup, FormControl, PageHeader, small } from 'react-bootstrap';
+import { Grid, Row, Col, ListGroup, ListGroupItem, FormGroup, FormControl, PageHeader, Button } from 'react-bootstrap';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 
 const socket = io.connect();
 
@@ -32,6 +32,15 @@ class App extends Component {
   }
 
   render() {
+    const data = [
+      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+    ];
     return (
       <div className="App">
         <div className="container-fluid">
@@ -48,17 +57,17 @@ class App extends Component {
                 </Col>
               </Row>
               <ListGroup className="stock-info">
-                <ListGroupItem className="row" >
+                <ListGroupItem className="row single-stock">
                   <Row>
                     <Col md={6}><code>APPL</code></Col><Col md={6}><code>$PRICE</code></Col>
                   </Row>
                 </ListGroupItem>
-                <ListGroupItem className="row">
+                <ListGroupItem className="row single-stock">
                   <Row>
                     <Col md={6}><code>APPL</code></Col><Col md={6}><code>$PRICE</code></Col>
                   </Row>
                 </ListGroupItem>
-                <ListGroupItem className="row">
+                <ListGroupItem className="row single-stock">
                   <Row>
                     <Col md={6}><code>APPL</code></Col><Col md={6}><code>$PRICE</code></Col>
                   </Row>
@@ -76,8 +85,22 @@ class App extends Component {
               </Row>
               <Row>
                 <Col md={12} className="live-chart">
-
+                  <LineChart width={1200} height={600} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+                    <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                  </LineChart>
                 </Col>
+              </Row>
+              <Row className="view-btn-section">
+                <Col md={2} className="view-btns"><Button>1 Day</Button></Col>
+                <Col md={2} className="view-btns"><Button>1 Week</Button></Col>
+                <Col md={2} className="view-btns"><Button>1 Month</Button></Col>
+                <Col md={2} className="view-btns"><Button>6 Months</Button></Col>
+                <Col md={2} className="view-btns"><Button>1 Year</Button></Col>
+                <Col md={2} className="view-btns"><Button>All</Button></Col>
               </Row>
             </Col>
           </Row>
