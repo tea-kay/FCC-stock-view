a7650cfcb1dc985af5d17c16a0c6901324599d23
Timothy Kim <tkim1202@gmail.com>
2018-02-26 18:46:25 -0500

    Added route for api endpoint

--- routes.js
+++ routes.js
@@ -10,3 +10,34 @@ module.exports = (io) => {
     })
   })
 }
+
+arr.myMap(function(item, idx) {
+  return item.toUpperCase()
+})
+
+Array.prototype.myMap = function(cb) {
+  var result = [];
+  for (var i = 0; i < this.length; i++) {
+    result.push(cb(this[i]))
+  }
+  return result;
+}
+
+// Initial Load
+Symbol.find({}).lean().exec((err, symbols) => {
+  if (err) {
+    socket.emit('errorMessage', { msg: 'Error reading from database' })
+  } else if (!symbols.length) {
+    socket.emit('errorMessage', { msg: 'There are no symbols in the database' })
+  } else {
+    const tickers = symbols.map(({ symbol }) => symbol)
+    const lte = moment().format('YYYYMMDD')
+    const gte = moment().subtract(1, 'months').format('YYYYMMDD')
+    const quandl = `https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?qopts.columns=ticker,date,close&date.gte=${gte}&date.lte=${lte}&ticker=${tickers.join(',')}&api_key=${process.env.QUANDL_API}`
+
+    fetch(quandl).then(response => response.json()).then(({ datatable }) => {
+      const { data } = datatable
+      socket.emit('init', { data, tickers })
+    })
+  }
+})
